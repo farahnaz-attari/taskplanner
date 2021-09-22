a652ab7346ba518337a16974aaf5710883b325eb
LAPTOP-4LKOIS89\FARAHNAZ <attari_farahnaz@yahoo.com>
2021-09-23 09:44:53 +1000

    practice

--- JS/taskManager.js
+++ JS/taskManager.js
@@ -15,16 +15,17 @@ class TaskManager {
             status1: status1,
             date: date
         };
-        this.tasks.push(newTask);
+        this._tasks.push(newTask);
 
 
     }
 
-    get tasks(){
-        return this._tasks;
-    }
+    // get tasks(){
+    //     return this._tasks;
+    // }
 
 }
+
 
 
 // taskList.addTask(myName.value, floatingTextarea2.value, assign.value, status1.value, date.value);
