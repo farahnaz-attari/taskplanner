1bed2575269edff5bb5f2591ae0780aa890f23ba
LAPTOP-4LKOIS89\FARAHNAZ <attari_farahnaz@yahoo.com>
2021-09-22 17:25:46 +1000

    adding task 4 and 5

---
+++ JS/taskManager.js
@@ -0,0 +1,33 @@
+class TaskManager {
+    constructor(currentId = 0){
+        this._tasks = [];
+        this.currentId = currentId;
+    }
+    
+    //function to add task
+    addTask(myName, floatingTextarea2, assign, status1, date){
+             
+        const newTask = {
+            id: this.currentId++,
+            myName: myName,
+            floatingTextarea2: floatingTextarea2,
+            assign: assign,
+            status1: status1,
+            date: date
+        };
+        this.tasks.push(newTask);
+
+
+    }
+
+    get tasks(){
+        return this._tasks;
+    }
+
+}
+
+
+// taskList.addTask(myName.value, floatingTextarea2.value, assign.value, status1.value, date.value);
+// console.log(taskList.tasks);
+
+
