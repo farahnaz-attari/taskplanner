d9fa4042009c3c8af969df5667435a0b583f81c6
LAPTOP-4LKOIS89\FARAHNAZ <attari_farahnaz@yahoo.com>
2021-10-05 16:24:57 +1100

    task 10 is done

--- JS/index.js
+++ JS/index.js
@@ -11,8 +11,16 @@ const  assign = document.querySelector('#assign');
 const  status1 = document.querySelector('#status');
 const  date = document.querySelector('#date');
 let validationFail = 0;
- const submit = document.querySelector('#submit');
- const doneButton = document.querySelector(".done-button");
+const submit = document.querySelector('#submit');
+const doneButton = document.querySelector(".done-button");
+
+//edit Modal variable
+const edit_myName = document.querySelector('#edit_myName');
+const edit_floatingTextarea2 = document.querySelector('#edit_floatingTextarea2');
+const edit_assign = document.querySelector('#edit_assign');
+const edit_status = document.querySelector('#edit_status');
+const edit_date = document.querySelector('#edit_date');
+const Update = document.querySelector('#Update');
  
  
 const validFormFieldInput = (event) => {
@@ -127,9 +135,105 @@ event.preventDefault();
      
   }
 
+  //editValidFormFieldInput function
+  const editValidFormFieldInput = event => {
+    event.preventDefault();
+     //checking task name//
+     if (edit_myName.value.length > 5) {
+      edit_myName.classList.add("is-valid");
+      edit_myName.classList.remove("is-invalid");
+    } else {
+      edit_myName.classList.add("is-invalid");
+      edit_myName.classList.remove("is-valid");
+       validationFail++;
+    }
+
+  //end task name
+  //check description//
+  if ( edit_floatingTextarea2.value.length > 5) {
+    edit_floatingTextarea2.classList.add("is-valid");
+    edit_floatingTextarea2.classList.remove("is-invalid");
+  } else {
+    edit_floatingTextarea2.classList.add("is-invalid");
+    edit_floatingTextarea2.classList.remove("is-valid");
+     validationFail++;
+  }
+//end discription//
+
+ //check assign//
+ if (edit_assign.value.length > 5) {
+  edit_assign.classList.add("is-valid");
+  edit_assign.classList.remove("is-invalid");
+} else {
+  edit_assign.classList.add("is-invalid");
+  edit_assign.classList.remove("is-valid");
+   validationFail++;
+}
+
+//end assign//
+
+//check status//
+if (edit_status.value !== 'Choose') {
+  edit_status.classList.add("is-valid");
+  edit_status.classList.remove("is-invalid");
+} else {
+  edit_status.classList.add("is-invalid");
+  edit_status.classList.remove("is-valid");
+   validationFail++;
+}
+//end status//
+
+
+      // check due date
+
+      if (edit_date.value){
+        edit_date.classList.add("is-valid");
+        edit_date.classList.remove("is-invalid");
+      } else {
+        edit_date.classList.add("is-invalid");
+        edit_date.classList.remove("is-valid");
+         validationFail++;
+      }
+     //end due date
+
+ 
+     if (validationFail > 0) {
+      validationFail = 0;
+      //clearFormFields();
+      return;
+    } else {
+      // Push the valid input into our tasks array
+      taskManager.updateTask(
+        edit_myName.value,
+        edit_floatingTextarea2.value,
+        edit_assign.value,
+        edit_status.value,
+        edit_date.value,
+        edit_taskId.value
+  
+      );
+      taskManager.save();
+      taskManager.render();
+      // clearFormFields();
+      $("#edit-exampleModal").modal("hide");
+    }
+      
+     
+  }
+
+
+
+
+
+
+
+
   
 
  submit.addEventListener('click',validFormFieldInput);
+ 
+
+ 
 
  const taskList = document.querySelector(".task-list");
 
@@ -153,6 +257,29 @@ event.preventDefault();
    }
    taskManager.save();
    taskManager.render();
+
+   //delete task 
+    if(event.target.classList.contains("delete-button")){
+      const parentTask =
+    event.target.parentElement.parentElement.parentElement.parentElement;
+    const taskId = Number(parentTask.dataset.taskId);
+    //let task = taskManager.getTaskById(taskId);
+    taskManager.delete(taskId);
+    }
+    // taskManager.save();
+    // taskManager.render();
+    if(event.target.classList.contains("edit-button")){
+      const parentTask =
+      event.target.parentElement.parentElement.parentElement.parentElement;
+      const taskId = Number(parentTask.dataset.taskId);
+      taskManager.editTask(taskId);
+      let task = taskManager.getTaskById(taskId);
+   //Update button
+   Update.addEventListener('click', editValidFormFieldInput);
+     
+    }
+    taskManager.save();
+    taskManager.render();
    
  });
 
